3d5bacbc0ea27472352eb8b7ec5389c7551d8afc
Sandra Aguti <agutisandra@gmail.com>
2022-09-08 14:31:44 -0700

    this is corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
-
-
 /**
 
  * main - prints exactly "and that piece of art is useful"
@@ -15,13 +11,9 @@
  * Return: Always 0 (Success)
 
  */
-
 int main(void)
-  
 {
-  
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  
-  return (1);
-  
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);  
+return (1);
 }
+
