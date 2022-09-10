221116b5b1c41c42136ab05bd9ed2dfeaf3b2185
Sandra Aguti <agutisandra@gmail.com>
2022-09-09 17:22:35 -0700

    variable corrections

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int n, m, l;
-for (n = 48; n < 58; n++)   
+for (n = 48; n < 58; n++)
 {
 for (m = 49; m < 58; m++)
 {
@@ -15,17 +15,17 @@ for (l = 50; l < 58; l++)
 if (l > m && m > n)
 {
 putchar(n);
-putchar(m);		  
+putchar(m);
 putchar(l);
 if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
 }
-} 
 }
-} 
+}
+}
 }
 putchar('\n');
-return (0); 
+return (0);
 }
