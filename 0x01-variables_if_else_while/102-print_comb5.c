0c1cf091cd2df34eda7bce388328c92b93b662a7
Sandra Aguti <agutisandra@gmail.com>
2022-09-09 17:34:09 -0700

    variable corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-int i, j; 
+int i, j;
 for (i = 0; i < 100; i++)
 {
 for (j = 0; j < 100; j++)
@@ -14,15 +14,15 @@ if (i < j)
 {
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
- putchar(' ');
+putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
 if (i != 98 || j != 99)
 {
-putchar(',');	  
-putchar(' '); 
+putchar(',');
+putchar(' ');
 }
-}  
+} 
 }
 }
 putchar('\n');
