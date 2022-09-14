f4091f819c522a3994a58defd312cbb4933fdf2f
Sandra Aguti <agutisandra@gmail.com>
2022-09-14 14:01:33 -0700

    debugging corections

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))    
+if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))
 {
 if (month >= 3 && day >= 60)
 {
@@ -28,6 +28,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-} 
-}  
+}
+}
 }
