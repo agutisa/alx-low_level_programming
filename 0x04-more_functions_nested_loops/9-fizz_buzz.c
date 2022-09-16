8c86b135d14bf8b516bb6f86425c997bf6652303
Sandra Aguti <agutisandra@gmail.com>
2022-09-15 18:29:36 -0700

    more nested

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
  /**
@@ -15,16 +14,13 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-}
-else if (i % 5 == 0 && i % 3 != 0)
+} else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
+} else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-}
-else if (i == 1)
+} else if (i == 1)
 {
 printf("%d", i);	  
 }
@@ -34,6 +30,5 @@ printf(" %d", i);
 }
 }
 printf("\n");
-
 return (0);
 }
