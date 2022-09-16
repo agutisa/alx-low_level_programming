fb68b3c28d8599091d0a633d1faeb9c091abc35a
Sandra Aguti <agutisandra@gmail.com>
2022-09-15 18:16:11 -0700

    more nested

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -5,6 +6,7 @@
  *@n: parameter
  *Return: returns nothing
  */
+
 void print_diagonal(int n)
 {
 if (n <= 0)
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,6 @@
 int main(void)
 {
 int i;
-
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
@@ -37,6 +36,5 @@ printf(" %d", i);
 }
 }
 printf("\n");
-
 return (0);
 }
