f208dda5b89c355d0b4bd8132b59346ac2f9b630
Sandra Aguti <agutisandra@gmail.com>
2022-09-19 12:10:15 -0700

    advanced corrections

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@ while (s[i] != '\0')
 {
 i++;
 }
- 
+
 len = i;
 
 for (j = len - 1; j >= 0; j--)
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 int len, i;
 
 len = 0;
- 
+
 while (src[len] != '\0')
 {
 len++;
@@ -23,6 +23,6 @@ for (i = 0; i < len; i++)
 dest[i] = src[i];
 }
 dest[i] = '\0';
- 
-return (dest);  
+
+return (dest);
 }
