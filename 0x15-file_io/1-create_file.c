a45e919b2dd74e15480cbf56d7497a60d8506568
agutisa <agutisandra@gmail.com>
2022-11-01 04:22:08 +0300

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,39 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <fcntl.h>
+
 /**
-  *create_file - A function that create a file
-  *@filename: name of the file
-  *@text_content : the context of the file
-  *Return: 1
-  */
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
-int fdo, fdw, len;
-if (filename == NULL)
-return (-1);
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
-fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (fdo == -1)
-return (-1);
-fdw = write(fdo, text_content, len);
-if (fdw == -1)
-return (-1);
-return (1);
-}
+
